083b4ccc7666512e836d962d4224fb74f3fe48b6
Antonio <antoniojosedonishung@gmail.com>
2022-07-19 16:55:21 -0500

    On demand update of home

--- home/home.go
+++ home/home.go
@@ -2,14 +2,13 @@ package home
 
 import (
 	_ "embed"
-	"github.com/shoriwe/plasma-website/markdown"
 	"github.com/shoriwe/plasma-website/templates"
 	"github.com/spf13/afero"
-	"io"
-	"net/http"
+	"html/template"
 )
 
-const readmeURL = "https://raw.githubusercontent.com/shoriwe/gplasma/main/README.md"
+//go:embed index.html
+var script template.HTML
 
 func Write(output afero.Fs) error {
 	file, createError := output.Create("index.html")
@@ -17,19 +16,10 @@ func Write(output afero.Fs) error {
 		return createError
 	}
 	defer file.Close()
-	response, requestError := http.Get(readmeURL)
-	if requestError != nil {
-		return requestError
-	}
-	defer response.Body.Close()
-	readme, readError := io.ReadAll(response.Body)
-	if readError != nil {
-		return readError
-	}
 	return templates.Render(
 		templates.Page{
 			Title: "Home",
-			Body:  markdown.Render(readme),
+			Body:  script,
 		},
 		file)
 }
